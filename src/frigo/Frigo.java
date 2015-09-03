234c3e59a00fb0a17cd5b73ca228d54eb204e0bc
tapersesmarrant <a.courand@hotmail.fr>
2015-09-03 08:49:09 +0200

    Correction frigo

--- src/frigo/Frigo.java
+++ src/frigo/Frigo.java
@@ -1,22 +1,31 @@
 package frigo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Frigo {
 
-	ArrayList<Aliment> aliments;
+	ArrayList<Aliment> listeAliments;
 	
 	public Frigo() {
-		this.aliments = new ArrayList<Aliment>();
+		this.listeAliments = new ArrayList<Aliment>();
 	}
 	
-	public ArrayList<Aliment> getAliments() {
-		return aliments;
+	public ArrayList<Aliment> getListeAliments() {
+		return listeAliments;
 	}
 	
-	public void setAliments(ArrayList<Aliment> aliments) {
-		this.aliments = aliments;
+	public void setAliments(Aliment aliment) {
+		this.listeAliments.add(aliment);
 	}
 	
-	
+	public String[] alimentCSV(){ 
+		int compteur;
+		File file = new File("");
+		//String chemin = 
+	}
 }
