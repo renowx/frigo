68602feb27509833716e876f5ab0f4f2edc726d4
kancurzv <valentin.kancurzewski@hotmail.com>
2015-09-03 09:09:56 +0200

    Update Main.java

--- src/frigo/Main.java
+++ src/frigo/Main.java
@@ -1,5 +1,29 @@
 package frigo;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Date;
 
+
+public class Main {
+	
+	public static void main (String [] arg  ){
+		
+		 Frigo frigo = new Frigo();
+		 Aliment tomate = new Aliment("tomate", "legume", Date(2015, 12, 6), 4, "");
+		 Aliment eggs = new Aliment("eggs", "viande", Date(2015, 9, 10), 4, "");
+		 Aliment beurre = new Aliment("beurre", "laitier", Date(2015, 10, 6), 250, "g");
+		 
+		 frigo.setAliments(tomate);
+		 frigo.setAliments(eggs);
+		 frigo.setAliments(beurre);
+		 
+		 
+		 
+	}
+
+	private static Date Date(int i, int j, int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
+
