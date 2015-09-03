04167ca5a75a8418fc35aba8a1a5aaf793a426b0
kancurzv <valentin.kancurzewski@hotmail.com>
2015-09-03 11:09:56 +0200

    Update Recette.java

--- src/frigo/Recette.java
+++ src/frigo/Recette.java
@@ -1,5 +1,52 @@
 package frigo;
 
-public class Recette {
+import java.util.ArrayList;
 
+public class Recette {
+	String nom;
+	
+	ArrayList<Aliment> listeAliments;
+	
+	public Recette(String nom, ArrayList<Aliment> listeAliments){
+		this.listeAliments = listeAliments;
+		this.nom = nom;
+	}
+	
+	public String toString(){	
+		String aliments = "";
+		for (Aliment aliment : listeAliments) {
+			aliments = aliments + aliment.toString() + "\n";
+		}
+		return this.nom + " Ingrédients : " + aliments;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public ArrayList<Aliment> getListeAliments() {
+		return listeAliments;
+	}
+	
+	public boolean recetteValide(Frigo frigo){
+		boolean res=false;
+		for (Aliment aliment : listeAliments) {
+			for(Aliment aliFrigo : frigo.getListeAliments()){
+				if(aliFrigo.getNom().equals(aliment.getNom())){
+					if(aliFrigo.getQuantite()>=aliment.getQuantite()){
+						res = true;
+					}
+					else{
+						return false;
+					}
+				}
+			}
+		}
+		return res;
+	}
+	
+	
+	
+	
 }
+
