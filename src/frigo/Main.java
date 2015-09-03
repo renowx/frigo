d4391dfc32d27df2819d27c114610ac8dbe7c1f3
RenowxSiders <renaud.carpentier@gmx.fr>
2015-09-03 09:46:46 +0200

    lol

--- src/frigo/Main.java
+++ src/frigo/Main.java
@@ -9,24 +9,21 @@ public class Main {
 	public static void main (String [] arg  ){
 		
 		 Frigo frigo = new Frigo();
-		 Aliment tomate = new Aliment("tomate", "legume", Date(2015, 12, 6), 4, "");
-		 Aliment eggs = new Aliment("eggs", "viande", Date(2015, 9, 10), 4, "");
-		 Aliment beurre = new Aliment("beurre", "laitier", Date(2015, 10, 6), 250, "g");
+		 Aliment tomate = new Aliment("tomate", "legume", new Date(2015, 12, 6), 4, "");
+		 Aliment eggs = new Aliment("eggs", "viande", new Date(2015, 9, 10), 4, "");
+		 Aliment beurre = new Aliment("beurre", "laitier", new Date(2015, 10, 6), 250, "g");
 		 
 		 frigo.setAliments(tomate);
 		 frigo.setAliments(eggs);
 		 frigo.setAliments(beurre);
 		 
-		 tomate.toString();
+		 System.out.println(tomate.toString());
 		 beurre.toString();
 		 eggs.toString();
 		 
 		 
 	}
 
-	private static Date Date(int i, int j, int k) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
 
