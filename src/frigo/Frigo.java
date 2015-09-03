3d977f32f9a458e6ba6dfdc9b0df1b8fd69d195c
tapersesmarrant <a.courand@hotmail.fr>
2015-09-03 08:27:30 +0200

    FrigoInit

--- src/frigo/Frigo.java
+++ src/frigo/Frigo.java
@@ -1,5 +1,22 @@
 package frigo;
 
+import java.util.ArrayList;
+
 public class Frigo {
 
+	ArrayList<Aliment> aliments;
+	
+	public Frigo() {
+		this.aliments = new ArrayList<Aliment>();
+	}
+	
+	public ArrayList<Aliment> getAliments() {
+		return aliments;
+	}
+	
+	public void setAliments(ArrayList<Aliment> aliments) {
+		this.aliments = aliments;
+	}
+	
+	
 }
