575cb06583948184605d4ee5c5892a98bb91df3a
kancurzv <valentin.kancurzewski@hotmail.com>
2015-09-03 13:38:18 +0200

    Update TestRecette.java

--- srcTest/frigo/TestRecette.java
+++ srcTest/frigo/TestRecette.java
@@ -1,7 +1,41 @@
 package frigo;
 
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import frigo.Aliment;
+import frigo.Frigo;
+import frigo.Recette;
 
 public class TestRecette extends TestCase {
 
+	 
+	Aliment oeuf = new Aliment("egg", "viande", new Date(2015, 9, 10), 5, "");
+	Aliment beurre2 = new Aliment("beurre", "laitier", new Date(2015, 10, 6), 100, "g");
+	ArrayList<Aliment> Aliment = new ArrayList<Aliment>();
+	
+	Frigo frigo = new Frigo();
+	 Aliment eggs = new Aliment("egg", "viande", new Date(2015, 9, 10), 4, "");
+	 Aliment beurre = new Aliment("beurre", "laitier", new Date(2015, 10, 6), 250, "g");
+	 
+
+				
+		
+		public void testRecette(){
+			Aliment.add(oeuf);
+			Aliment.add(beurre2);
+			Recette omelette = new Recette("omelette", Aliment);
+			Assert.assertEquals("omelette Ingrédients : 5 egg(s) 100g beurre(s) ", omelette.toString());
+			frigo.setAliments(eggs);
+			frigo.setAliments(beurre);
+			Assert.assertFalse( omelette.recetteValide(frigo));
+
+		}
 }
