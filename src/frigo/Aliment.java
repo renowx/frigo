e7970a24e51d34a11d4f0596d834d33e0aab9df0
RenowxSiders <renaud.carpentier@gmx.fr>
2015-09-03 08:34:57 +0200

    1 jet bis2
    Type aliment créer

--- src/frigo/Aliment.java
+++ src/frigo/Aliment.java
@@ -1,5 +1,23 @@
 package frigo;
 
-public class Aliment {
+import java.util.Date;
 
+public class Aliment {
+	private String nom;
+	private String type;
+	private Date peremption;
+	private int quantite;
+	private String unite;
+	
+	public Aliment(String nom, String type, Date peremption, int quantite, String unite){
+		this.nom = nom;
+		this.type = type;
+		this.peremption = peremption;
+		this.quantite = quantite;
+		this.unite = unite;
+	}
+	
+	public String toString(){
+		return quantite + " " + unite + " " + nom + "(s)";
+	}
 }
