14adbc5e7dd38b1f121f9d367db1753ee8f63560
tapersesmarrant <a.courand@hotmail.fr>
2015-09-04 10:01:07 +0200

    Meain

---
+++ src/frigo/ActionMenu.java
@@ -0,0 +1,16 @@
+package frigo;
+
+public class ActionMenu {
+	
+	Frigo frigo ;
+
+	public ActionMenu(int ct, Frigo frigo) {
+		this.frigo = frigo;
+	}
+
+	public void consulterFrigo() {
+		
+	}
+	
+	
+}
